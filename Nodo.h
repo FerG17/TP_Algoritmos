1918d306ddd7f5690bd70fc572da44e95bd970bc
FerG17 <fguerec17@gmail.com>
2025-05-14 11:32:50 -0500

    Nodo.h

---
+++ Nodo.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef __NODO_H__
+#define __NODO_H__
+
+template<class T>
+class Nodo {
+public:
+    T valor;
+    Nodo<T>* siguiente;
+
+    Nodo(T v, Nodo<T>* sig = nullptr) : valor(v), siguiente(sig) {}
+};
+
+#endif
