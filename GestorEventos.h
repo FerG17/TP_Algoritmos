ec242c2852169b64c0d6c319152f3bede33075c7
FerG17 <fguerec17@gmail.com>
2025-06-30 11:09:56 -0500

    GestorEventos.h

--- GestorEventos.h
+++ GestorEventos.h
@@ -3,15 +3,17 @@
 #include "Evento.h"
 #include "Concierto.h"
 #include <stack>
+#include "Hash.h"
 
 
 class GestorEventos {
 private:
     Lista<Evento*> eventos;
     stack<Evento*> historial;
-
+    HashTabla hashEventos;
 
 public:
+	GestorEventos() : hashEventos(100) {}
     Lista<Evento*>& getEventos();
     void agregarEvento(Evento* e);
     void mostrarEventos();
@@ -19,6 +21,7 @@ public:
     void historialPush(Evento* e);
     void historialPop();
     bool historialVacio() const;
+    Evento* buscarPorId(int id);
     ~GestorEventos();
 };
 Lista<Evento*>& GestorEventos::getEventos() {
@@ -28,22 +31,28 @@ void GestorEventos::agregarEvento(Evento* e) {
     eventos.insertarOrdenado(e, [](Evento* a, Evento* b) {
         return a->getFecha() < b->getFecha();
         });
+    int idx = eventos.tamaño() - 1;
+    hashEventos.insertar(e->getId(), idx);
 }
 
 void GestorEventos::mostrarEventos() {
-    int y = 5; 
+    int y = 5;
     eventos.forEach([&y](Evento* e) {
-        gotoxy(10, y+= 5); 
+        gotoxy(10, y += 5);
         e->mostrar();
         });
 }
-
-
 
 Evento* GestorEventos::buscarPorMes(const string& mes) {
     return eventos.buscar([mes](Evento* e) {
         return e->getFecha().substr(5, 2) == mes;
         });
+}
+
+Evento* GestorEventos::buscarPorId(int id) {
+    int idx = hashEventos.buscar(id);
+    if (idx == -1) return nullptr;
+    return eventos.obtener(idx);
 }
 
 void GestorEventos::historialPush(Evento* e) {
